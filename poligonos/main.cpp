3ef918c9538585d610fad265367ae0f527800fd2
emmummu <112498836+emmummu@users.noreply.github.com>
2023-10-04 11:02:16 -0600

    main.cpp

--- poligonos/main.cpp
+++ poligonos/main.cpp
@@ -16,6 +16,15 @@ int main() {
     v[3].Set(0.0f, 1.0f);
     squareShape.Set(v, 4);
 
+ b2PolygonShape pentagonShape;
+b2Vec2 vertices[5];
+for (int i = 0; i < 5; ++i) {
+    float angle = i * (2 * b2_pi / 5);
+    vertices[i].Set(cos(angle), sin(angle));
+}
+pentagonShape.Set(vertices, 5);
+
+
     b2PolygonShape hexagonShape;
     b2Vec2 verticesh[6];
     for (int i = 0; i < 6; ++i) {
